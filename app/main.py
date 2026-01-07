edebbd22de22ee20d4e5b820b98998964babbb2f
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2026-01-07 14:05:36 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split(" ")) < 3:
+        return
+    command, filename_to_copy, filename_target = command.split(" ")
+    if command != "cp":
+        return
+    if filename_to_copy == filename_target:
+        return
+    try:
+        with open(filename_to_copy, "r") as file_to_copy, open(
+            filename_target, "w"
+        ) as file_to_write:
+            content = file_to_copy.read()
+            file_to_write.write(content)
+    except FileNotFoundError:
+        return
